5c640e099728ea952f8fb040c2dc310a4c5c9058
Josh Donner <jdonner@dsndata.com>
2018-04-19 07:44:40 -0500

    Fixed window jumping
    I know this is considered a bug in SublimeText, but the fix seemed
    simple enough. Thanks to fico for the code, which I found here:
    
    https://forum.sublimetext.com/t/change-language-lost-focus-annoying-behavior/18986/3

--- ExpandTabsOnSave.py
+++ ExpandTabsOnSave.py
@@ -6,11 +6,26 @@ class ExpandTabsOnSave(sublime_plugin.EventListener):
     """Expand tabs / spaces on file save."""
 
     def on_pre_save(self, view):
+        # Store the viewport position and selections.
+        window = view.window()
+        x, y = view.viewport_position()
+
+        selections = view.sel()
+        savedSelections = []
+
+        for selection in selections:
+            savedSelections.append(selection)
+
+        view.selection.clear()
+
         """Run ST's 'expand_tabs' command when saving a file."""
         if view.settings().get('convert_tabspaces_on_save') == 1:
             if view.settings().get('translate_tabs_to_spaces') == 1:
                 view.run_command('expand_tabs')
             else:
                 view.run_command('unexpand_tabs')
-            
 
+        # Restore the viewport position and selections.
+        window.focus_view(view)
+        view.set_viewport_position([0, y], animate=False)
+        view.selection.add_all(savedSelections)
