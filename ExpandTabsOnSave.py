74502bcd873dfb4492a2b0ce80fa30708d1745d3
Josh Donner <jdonner@dsndata.com>
2018-04-19 09:21:00 -0500

    Additional changes for groups.
    If you have the same file open in multiple groups, the previous fix
    wasn't enough, I think this is more robust now.

--- ExpandTabsOnSave.py
+++ ExpandTabsOnSave.py
@@ -8,24 +8,29 @@ class ExpandTabsOnSave(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         # Store the viewport position and selections.
         window = view.window()
-        x, y = view.viewport_position()
+        a_view = window.active_view()
+        group = window.active_group()
+        x, y = a_view.viewport_position()
 
-        selections = view.sel()
+        print('group: {0}'.format(str(group)))
+
+        selections = a_view.sel()
         savedSelections = []
 
         for selection in selections:
             savedSelections.append(selection)
 
-        view.selection.clear()
+        a_view.selection.clear()
 
         """Run ST's 'expand_tabs' command when saving a file."""
-        if view.settings().get('convert_tabspaces_on_save') == 1:
-            if view.settings().get('translate_tabs_to_spaces') == 1:
-                view.run_command('expand_tabs')
+        if a_view.settings().get('convert_tabspaces_on_save') == 1:
+            if a_view.settings().get('translate_tabs_to_spaces') == 1:
+                a_view.run_command('expand_tabs')
             else:
-                view.run_command('unexpand_tabs')
+                a_view.run_command('unexpand_tabs')
 
         # Restore the viewport position and selections.
-        window.focus_view(view)
-        view.set_viewport_position([0, y], animate=False)
-        view.selection.add_all(savedSelections)
+        window.focus_group(group)
+        window.focus_view(a_view)
+        a_view.set_viewport_position([0, y], animate=False)
+        a_view.selection.add_all(savedSelections)
